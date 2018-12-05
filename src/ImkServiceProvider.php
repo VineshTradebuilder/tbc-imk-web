80cf4527b3f9fe562da8bcc58e3c96eb7a070797
vinesh <vinesh@tradebuilderinc.com>
2018-12-05 11:25:56 +0530

    add test functions

--- src/ImkServiceProvider.php
+++ src/ImkServiceProvider.php
@@ -2,7 +2,34 @@
 namespace TBC\IMK\WEB;
 
 class ImkServiceProvider{
-    
+    private $api_url;
+    private $api_key;    
+    private $api_user;
+    private $api_group;
+
+    function test(){
+        echo "test successfully.";
+    }
+    function setApiUrl( $url ){
+        $this->api_url = $url;
+    }
+    function setApiKey( $key ){
+        $this->api_key = $key;
+    }
+    function setApiUser( $user ){
+        $this->api_user = $user;
+    }
+    function setApiGroup( $group ){
+        $this->api_group = $group;
+    }
+    function printDetail(){
+        print_r( [
+            $this->api_url, 
+            $this->api_key,
+            $this->api_user,
+            $this->api_group
+        ] );
+    }
 }
 
 ?>
