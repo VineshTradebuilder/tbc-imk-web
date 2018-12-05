1947ba7a0b1c1550b157325e7f1aa3c652a0e69a
vinesh <vinesh@tradebuilderinc.com>
2018-12-05 12:32:55 +0530

    added few functions

--- src/ImkServiceProvider.php
+++ src/ImkServiceProvider.php
@@ -1,35 +1,443 @@
 <?php
+
 namespace TBC\IMK\WEB;
 
-class ImkServiceProvider{
+class ImkServiceProvider {
+
     private $api_url;
-    private $api_key;    
+    private $api_key;
     private $api_user;
     private $api_group;
+    private $client;
 
-    function test(){
-        echo "test successfully.";
-    }
-    function setApiUrl( $url ){
+    function setApiUrl($url) {
         $this->api_url = $url;
+        return $this;
     }
-    function setApiKey( $key ){
+
+    function setApiKey($key) {
         $this->api_key = $key;
+        return $this;
     }
-    function setApiUser( $user ){
+
+    function setApiUser($user) {
         $this->api_user = $user;
+        return $this;
     }
-    function setApiGroup( $group ){
+    
+
+    function setApiGroup($group) {
         $this->api_group = $group;
+        return $this;
     }
-    function printDetail(){
-        print_r( [
-            $this->api_url, 
-            $this->api_key,
-            $this->api_user,
-            $this->api_group
-        ] );
+
+    function init() {
+        if (empty($this->api_url)) {
+            throw new Exception("IMK API URI is required.");
+        }
+        
+        if (empty($this->api_user)) {
+            throw new Exception("IMK API User is required.");
+        }
+
+        if (empty($this->api_group)) {
+            throw new Exception("IMK API Group is required.");
+        }
+
+        $this->client = new Client([
+            "base_uri" => $this->api_url
+        ]);
     }
+
+    function getBlogs($params = []) {
+        $queryStr = '';
+        if (count($params)) {
+            $queryStr = "?" . http_build_query($params);
+        }
+
+        $blogurl = "api/v1/posts/" . $this->api_user . "/" . $this->api_group . $queryStr;
+
+        return $this->client->request('GET', $blogurl, [], ['withSuccess' => true]);
+    }
+
+    function getRecentPosts() {
+        $fp = $this->client->request('GET', "api/v1/posts/recent/" . $this->api_user . "/" . $this->api_group);
+        return $fp->posts;
+    }
+
+    function getCategories() {
+        return $this->client->request('GET', "api/v1/posts/categories/" . $this->api_user . "/" . $this->api_group
+        );
+    }
+
+    function getArchives() {
+        return $this->client->request('GET', "api/v1/posts/archives/" . $this->api_user . "/" . $this->api_group);
+    }
+
+    function getSingleBlog($postId) {
+        return $this->client->request('GET', "api/v1/post/" . $postId . "/" . $this->api_user . "/" . $this->api_group);
+    }
+
+    function getFeaturedProperties() {
+        $data['orgId'] = $this->api_group;
+        $data['userId'] = $this->api_user;
+        $data['type'] = 'photo';
+
+        return $this->client->request('POST', 'api/getFeaturedProperties', ['form_params' => $data]);
+    }
+
+    function getAgents() {
+        $data = ["fetchFor" => "agent", "orgId" => $this->api_group, "userId" => $this->api_user];
+        return $this->client->request('post', "api/readMembers", ['form_params' => $data], ['withSuccess' => true]);
+    }
+
+    function getleadership() {
+        $data = ["fetchFor" => "leadership", "orgId" => $this->api_group, "userId" => $this->api_user];
+
+        return $this->client->request(
+                        'post', "api/readMembers", ['form_params' => $data], ['withSuccess' => true]
+        );
+    }
+
+    function singleAgent($aId) {
+        return $this->client->request(
+                        'GET', "api/readMember/" . $aId, [], ['withSuccess' => true]
+        );
+    }
+
+    function getAgent($license) {
+        try {
+            $url = 'https://prod.imkloud.com/api/getAgentInfo/' . $license;
+            $fp = $this->client->request('GET', $url);
+            if ($body = $fp->getBody()) {
+                return json_decode($body);
+            }
+        } catch (ClientException $e) {
+            return [];
+        } catch (RequestException $e) {
+
+            return [];
+        } catch (TransferException $e) {
+
+            return [];
+        }
+    }
+
+    function getProperties($filters) {
+        $data['userId'] = $this->api_user;
+        $data['orgId'] = $this->api_group;
+        $data['type'] = 'photo';
+        $data['limit'] = $this->limit;
+        if (isset($filters['currentPage'])) {
+            $data['skip'] = ($filters['currentPage'] - 1 ) * $this->limit;
+        } else {
+            $data['skip'] = 0;
+        }
+
+        $data['filter'] = $filters;
+
+        return $this->client->request('POST', 'api/getAll/properties', ['json' => $data]);
+    }
+
+    function comingSoon($filters) {
+        $data['userId'] = $this->api_user;
+        $data['orgId'] = $this->api_group;
+        $data['type'] = 'photo';
+        $data['limit'] = $this->limit;
+
+        if (isset($filters['currentPage'])) {
+            $data['skip'] = ($filters['currentPage'] - 1 ) * $this->limit;
+        } else {
+            $data['skip'] = 0;
+        }
+
+        $data['filter'] = $filters;
+        return $this->client->request('POST', 'api/getUpcomingProperties', ['json' => $data]);
+    }
+
+    function getSingleProperty($pId, $mlsId) {
+        $params = [
+            "_id" => $pId,
+            "listingId" => $mlsId,
+            "orgId" => $this->api_group,
+            "userId" => $this->api_user
+        ];
+        $fp = $this->client->request(
+                'POST', 'api/getSingle/properties', ['json' => $params]
+        );
+        if (count($fp)) {
+            return $fp[0];
+        } else {
+            return [];
+        }
+    }
+
+    function getSingleMyListing($pId, $mlsId) {
+        $params = [
+            "_id" => $pId,
+            "listingId" => $mlsId,
+            "orgId" => $this->api_group,
+            "userId" => $this->api_user
+        ];
+        $fp = $this->client->request(
+                'POST', 'api/getSingleMyListing/properties', ['json' => $params]
+        );
+        if (count($fp)) {
+            return $fp[0];
+        } else {
+            return [];
+        }
+    }
+
+    function getSinglePropertyComingsoon($pId, $mlsId) {
+        $params = [
+            "_id" => $pId,
+            "listingId" => $mlsId,
+            "orgId" => $this->api_group,
+            "userId" => $this->api_user
+        ];
+        $fp = $this->client->request(
+                'POST', 'api/getSingleMyListing/properties', ['json' => $params]
+        );
+        if (count($fp)) {
+            return $fp[0];
+        } else {
+            return [];
+        }
+    }
+
+    function greatSchool($street, $city, $state) {
+        try {
+
+            if (!$street) {
+                throw new Exception("Please provice street or locality.");
+            }
+
+            if (!$city) {
+                throw new Exception("Please provice city.");
+            }
+
+            if (!$state) {
+                throw new Exception("Please provice state.");
+            }
+            $street = explode(",", $street);
+            $street = reset($street);
+            $config = [
+                "key" => Config::get('great_school/key'),
+                "limit" => 10,
+                'address' => $street,
+                'city' => $city,
+                "state" => $state,
+                "radius" => "30",
+                "schoolType" => "public-private"
+            ];
+
+            $url = Config::get('great_school/url') . '/nearby?' . http_build_query($config);
+
+            $data = $this->client->request("GET", $url, [], ["raw" => true]);
+
+            $arryaData = (array) simplexml_load_string($data);
+
+            if ($arryaData) {
+                return $arryaData['school'];
+            }
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    function wiki($city, $state) {
+        try {
+            $state = Helper::state_abbr($state, 'name');
+            $city = Helper::un_slug($city, true);
+            $params = [
+                "format" => 'json',
+                "action" => "query",
+                "prop" => 'extracts',
+                "exintro" => "explaintext",
+                "titles" => $city . ", " . $state
+            ];
+            $queryStr = '';
+            if (count($params)) {
+                $queryStr = "?" . http_build_query($params);
+            }
+
+            $url = 'http://en.wikipedia.org/w/api.php' . $queryStr;
+
+            $data = $this->client->request("Get", $url);
+            if ($data && Helper::input($data, 'query') && Helper::input($data->query, 'pages')) {
+                return reset($data->query->pages);
+            } else {
+                return [];
+            }
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    /*
+      @param $address
+     */
+
+    function getNormalizedCity($city) {
+        try {
+            $fields = [
+                "locality", "administrative_area_level_1"
+            ];
+
+            $config = [
+                'address' => $city,
+                'sensor' => false,
+                'key' => Config::get('google/key')
+            ];
+
+            $queryStr = '';
+            if (count($config)) {
+                $queryStr = "?" . http_build_query($config);
+            }
+
+            $url = "https://maps.googleapis.com/maps/api/geocode/json" . $queryStr;
+
+            $data = $this->client->request("Get", $url);
+
+            $address = [];
+            if (Helper::input($data, 'status') == 'OK' && Helper::input($data, 'results') && isset($data->results[0])) {
+
+                foreach ($data->results[0]->address_components as $address_component) {
+                    if (Helper::input($address_component, 'types') && isset($address_component->types[0])) {
+                        if (in_array($address_component->types[0], $fields)) {
+                            switch ($address_component->types[0]) {
+                                case 'locality':
+                                    $address["city"] = $address_component->long_name;
+                                    break;
+                                case 'administrative_area_level_1':
+                                    $address["state"] = $address_component->long_name;
+                                    break;
+                                case 'administrative_area_level_2':
+                                    $address["address"] = $address_component->long_name;
+                                    break;
+                                case 'country':
+                                    $address["country"] = $address_component->long_name;
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                if (Helper::input($data->results[0], 'geometry') && Helper::input($data->results[0]->geometry, 'location')) {
+                    $address['location'] = (array) Helper::input($data->results[0]->geometry, 'location');
+                }
+                if (Helper::input($data->results[0], 'formatted_address')) {
+                    $address['full_address'] = $data->results[0]->formatted_address;
+                }
+            }
+            return ($address);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    function walkScore($address, $lat, $lng) {
+        try {
+
+            $config = [
+                'format' => 'json',
+                'address' => $address,
+                'lat' => $lat,
+                'lon' => $lng,
+                'transit' => 1,
+                'bike' => 1,
+                'wsapikey' => Config::get('ws/key')
+            ];
+
+            $queryStr = '';
+            if (count($config)) {
+                $queryStr = "?" . http_build_query($config);
+            }
+
+            $url = 'http://api.walkscore.com/score' . $queryStr;
+            $data = $this->client->request("Get", $url);
+            return $data;
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    /*
+      @param [zip=> 132132, country_code=>"xx" ]]
+      @param [geo=>[lat=>xxx,lon=>xxx]]
+      @param [address=>"xxxx"]
+     */
+
+    function weather($address) {
+        try {
+            $config = [
+                'units' => 'metric',
+                'cnt' => 7,
+                'APPID' => Config::get('weather/key')
+            ];
+
+            if (isset($address['address'])) {
+                $config['q'] = $address['address'];
+            } else if (isset($address['geo'])) {
+                $config['lat'] = $address['geo']['lat'];
+                $config['lon'] = $address['geo']['lng'];
+            } else if (isset($address['zip'])) {
+                if (!isset($address['country_code'])) {
+                    throw new Exception("Country code required");
+                }
+                $config['zip'] = $address['zip'] . "," . $address['country_code'];
+            }
+
+            $queryStr = '';
+            if (count($config)) {
+                $queryStr = "?" . http_build_query($config);
+            }
+
+            $url = 'http://api.openweathermap.org/data/2.5/weather' . $queryStr;
+            $data = $this->client->request("Get", $url);
+            return $data;
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    private function yelp_request($address) {
+        try {
+            $unsigned_url = Config::get('yelp/API_HOST') . $address;
+            $token = new OAuthToken(Config::get('yelp/TOKEN'), Config::get('yelp/TOKEN_SECRET'));
+            $consumer = new OAuthConsumer(Config::get('yelp/CONSUMER_KEY'), Config::get('yelp/CONSUMER_SECRET'));
+            $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+            $oauthrequest = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $unsigned_url);
+
+            // Sign the request
+            $oauthrequest->sign_request($signature_method, $consumer, $token);
+
+            // Get the signed URL
+            $signed_url = $oauthrequest->to_url();
+
+            $data = $this->client->request("Get", $signed_url);
+            return $data;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    function yelp_search($term, $location) {
+
+        try {
+            if (!$term) {
+                throw new Exception("Please find term (banks, schools, food and restaurants etc.), this field is required!");
+            }
+            $url_params = ['term' => $term, 'location' => $location, "limit" => Config::get('yelp/SEARCH_LIMIT')];
+            $search_path = "search?" . http_build_query($url_params);
+
+            return $this->yelp_request($search_path);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
 }
 
 ?>
